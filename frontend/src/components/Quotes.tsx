d6209a11a020480f8647ee94f80a365bbe36f511
Kuldeep Gaud <140884017+Deepanshudks@users.noreply.github.com>
2024-12-18 22:33:21 +0530

    commit

--- frontend/src/components/Quotes.tsx
+++ frontend/src/components/Quotes.tsx
@@ -3,7 +3,7 @@ export const Quotes = () =>{
         <div className="flex justify-center items-center flex-col ">
             <div className="max-w-2xl">
             <div className="flex text-2xl font-bold items-center m-4 justify-center">
-                "The cutomer service I recieved was exceptional. The support team went abive and beyond to address my concerns."
+                "The cutomer service I recieved was exceptional. The support team went above and beyond to address my concerns."
             </div>
             <div className="font-bold ml-6">Julies Winnfield</div>
             <div className="font-medium text-zinc-500 ml-6">CEO, Acme Inc</div>
